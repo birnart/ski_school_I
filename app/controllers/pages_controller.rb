9930e5d65cc53ae56413a05b609927dce4bb57ab
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2021-02-16 14:45:48 +0100

    free tab

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
   end
 
-  def team
+  def free_tab
   end
 
   def kontakt
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'team', to: 'pages#team'
+  get 'free_tab', to: 'pages#free_tab'
   get 'kurse', to: 'pages#kurse'
   get 'kontakt', to: 'pages#kontakt'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
