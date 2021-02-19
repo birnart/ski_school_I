97515f7e4cf5850ca82952ea558b45a6bdb2962e
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2021-02-19 13:18:44 +0100

    keks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { cardInfoToggler } from '../components/card_toggler';
+import { cardInfoToggler } from './cardtoggler';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
---
+++ app/javascript/packs/cardtoggle.js
@@ -0,0 +1,11 @@
+const cardInfoToggler = () => {
+  const toggler = document.getElementById('toggler');
+  // const infoToggle2 = document.getElementById("toggle-element");
+
+  toggler.addEventListener('click', (event) => {
+    console.log(event);
+  });
+};
+
+export { cardInfoToggler };
+
